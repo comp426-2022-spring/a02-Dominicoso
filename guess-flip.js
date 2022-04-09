81ae3dbd4b7c8549424fb5a13b27ae5ef29e5a7c
Dominicoso <dominicoso@gmail.com>
2022-04-09 12:48:21 -0400

    guess flip with no error handling

--- guess-flip.js
+++ guess-flip.js
@@ -1,5 +1,10 @@
+import 'coin.mjs'
+
+const { coinFlip } = require('./modules/coin.mjs')
+
 const args = require('minimist')(process.argv.slice(2))
 args["call"]
 
 // add an error message here if argument is not 'head' or 'tails'
 
+console.log(flipACoin(args.call))
