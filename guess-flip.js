e5650220056df120c7919b0a0b0f378f11b6e9ab
Dominicoso <dominicoso@gmail.com>
2022-04-14 14:39:29 -0400

    error added

--- guess-flip.js
+++ guess-flip.js
@@ -9,10 +9,9 @@ const args = require('minimist')(process.argv.slice(2))
 let call = args["call"]
 
 // add an error message here if argument is not 'head' or 'tails'
-// if (args.call != 'heads' || 'tails') {
-//     console.error('no input')
-//     return
-// }
+if (call != 'heads' || 'tails') {
+    throw 'Error: no imput.'
+}
 
 
 
