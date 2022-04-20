fe25a7304277bad7c57dc8d489a8acae3bb2d726
Dominicoso <dominicoso@gmail.com>
2022-04-20 14:21:36 -0400

    added error handling

--- guess-flip.js
+++ guess-flip.js
@@ -1,18 +1,20 @@
 import {flipACoin} from './modules/coin.mjs'
 import {createRequire} from 'module'
+import { urlToHttpOptions } from 'url'
 
 const require = createRequire(import.meta.url)
 
 //var args = process.argv.slice(2)
 
 const args = require('minimist')(process.argv.slice(2))
-let call = args["call"]
+args["call"]
+
+let options = ['heads', 'tails']
 
 // add an error message here if argument is not 'head' or 'tails'
-if (call != 'heads' || 'tails') {
-    throw 'Error: no imput.'
+if (options.includes(args.call)) {
+    console.log(flipACoin(call))
+} else {
+    console.error('Error: no input.')
+    console.log('Usage: node guess-flip --call=[heads|heads]')
 }
-
-
-
-console.log(flipACoin(call))
