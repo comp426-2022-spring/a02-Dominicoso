552e688fab8a782348a4232ce4003905414fd753
Dominicoso <dominicoso@gmail.com>
2022-04-20 14:28:53 -0400

    changed or operator

--- flips.js
+++ flips.js
@@ -4,9 +4,9 @@ import {createRequire} from 'module'
 const require = createRequire(import.meta.url)
 
 const args = require('minimist')(process.argv.slice(2))
-const number = args["number"] || 1
+args["number"]
 
-let array = coinFlips(number)
+let array = coinFlips(args.number || 1)
 console.log(array)
 
 console.log(countFlips(array))
